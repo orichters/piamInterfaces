a606db48a3cf61939ce3f5a93225c2acce5a726c
orichters <oliverr@pik-potsdam.de>
2024-04-04 14:58:29 +0200

    fix AR6 problems, add feature to rename variables

--- R/renameOldVariables.R
+++ R/renameOldVariables.R
@@ -7,29 +7,29 @@
 #' @param logFile filename of file for logging
 #' @importFrom dplyr filter
 #' @importFrom rlang .data
-#' @importFrom quitte as.quitte calc_addVariable_
 #' @importFrom tibble as_tibble
 #' @return quitte object with adapted mif data
 #' @export
 
-addMissingVariables <- function(mifdata, variables, logFile = NULL) {
+renameOldVariables <- function(mifdata, variables, logFile = NULL) {
   mifdata <- as.quitte(mifdata)
 
   toadd <- unique(setdiff(variables, levels(mifdata$variable)))
-  formulas <- system.file("addMissingVariables.csv", package = "piamInterfaces") %>%
+  csvdata <- system.file("renamed_piam_variables.csv", package = "piamInterfaces") %>%
     read.csv2(comment.char = "#") %>%
     as_tibble() %>%
-    filter(.data$piam_variable %in% toadd)
+    filter(.data$piam_variable %in% toadd, .data$old_name %in% levels(mifdata$variable))
+  old2new <- csvdata$piam_variable
+  names(old2new) <- csvdata$old_name
   # we need to loop because the second calculation might need data from the first
-  for (i in seq_len(nrow(formulas))) {
-    liste <- list(c(formulas$formula[[i]], formulas$piam_unit[[i]]))
-    names(liste) <- formulas$piam_variable[[i]]
-    mifdata <- as.quitte(calc_addVariable_(mifdata, liste, only.new = FALSE, skip.missing.rhs = TRUE))
+  for (v in names(old2new)) {
+    mifdata <- mifdata %>%
+      mutate(variable = factor(ifelse(.data$variable == v, old2new[[v]], as.character(.data$variable))))
   }
 
-  added <- intersect(levels(mifdata$variable), toadd)
-  if (length(added) > 0 && ! is.null(logFile) && ! isFALSE(logFile)) {
-    logtext <- c("Automatically added variables: ", paste(added, collapse = ", "))
+  if (length(old2new) > 0 && ! is.null(logFile) && ! isFALSE(logFile)) {
+    logtext <- c("Automatically added variables based on renamed_piam_variables.csv:\n",
+                 paste(names(old2new), " -> ", old2new, "\n", collapse = ", "))
     write(logtext, file = logFile, append = TRUE)
   }
 
